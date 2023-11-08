40e208815da01dcf97119804c782f7c2997d8bd4
smheidrich <smheidrich@weltenfunktion.de>
2023-11-08 22:14:04 +0100

    Enable GitHub issues (Terraform)

--- terraform/github.tf
+++ terraform/github.tf
@@ -6,6 +6,7 @@ resource "github_repository" "project" {
   name        = var.project_name
   description = "${var.project_description} | mirror of https://gitlab.com/${gitlab_project.project.path_with_namespace}"
   visibility = "public"
+  has_issues = true
 }
 
 data "github_user" "current" {
